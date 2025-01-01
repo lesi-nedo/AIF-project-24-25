7feb15db8b11f641bbe4f6f23d070cb828b95eff
duddye <jogendy01@gmail.com>
2025-01-01 17:12:31 +0100

    minimax simulation implementation

---
+++ minimax/simulation/character.py
@@ -0,0 +1,3 @@
+class character:
+    def __init__(self, character_data):
+        self.data = character_data
--- minimax/simulation/simulation.py
+++ minimax/simulation/simulation.py
@@ -1,3 +1,6 @@
+from logging import currentframe
+from turtledemo.paint import switchupdown
+
 from pyftg.aiinterface.command_center import CommandCenter
 from pyftg.models.audio_data import AudioData
 from pyftg.models.frame_data import FrameData
@@ -20,19 +23,83 @@ class Simulator:
         self.characters = self.frame_data.character_data.copy()
         self.proj = self.frame_data.projectile_data.copy()
 
+    def detectHit(self, oppInd: int, attack):
+        pass
+
+    def hitPlayer(self, oppInd, attInd, attack, currentFrame):
+        self.proj = self.proj
+
     def processFight(self, currentFrame: int):
         self.processingCommands()
         self.processingHit(currentFrame)
-        self.updateAttackParameters()
+        self.updateAttackParameters(currentFrame)
         self.updateCharacters()
 
     def processingCommands(self):
+        self.proj = self.proj
 
     def processingHit(self, currentFrame: int):
+        isHit = [False, False]
 
-    def updateAttackParameters(self):
+        for p in self.proj:
+            oppInd = 1-p.player_number
+            if self.detectHit(oppInd, p):
+                i = 1 - oppInd
+                self.hitPlayer(oppInd,i,p,currentFrame)
+            else:
+                self.proj.append(p)
+
+        for i in range(0,2):
+            oppInd = 1 - i
+            attack = self.characters[i].attack_data
+            if self.detectHit(oppInd, attack):
+                isHit[oppInd] = True
+                self.hitPlayer(oppInd, i, attack, currentFrame)
+
+    def updateAttackParameters(self, currentFrame: int):
+        for p in self.proj:
+            p.active += 1
+            if not p.active < currentFrame:
+                self.proj.remove(p)
+            else:
+                p.current_hit_area.left += p.speed_x
+                p.current_hit_area.right+= p.speed_x
+                p.current_hit_area.top += p.speed_y
+                p.current_hit_area.bottom += p.speed_y
+
+        for i in range(0,2):
+            if not self.updateAttack(i):
+                self.characters[i].attack_data = None
 
     def updateCharacters(self):
+        for i in range(0,2):
+            self.characters[i].x += self.characters[i].speed_x
+            self.characters[i].y += self.characters[i].speed_y
+            if self.characters[i].bottom >= 640:
+                self.characters[i].speed_x += -self.characters[i].speed_x/abs(self.characters[i].speed_x)
+                self.characters[i].speed_y = 0
+            else:
+                if self.characters[i].top <= 0:
+                    self.characters[i].speed_y = 1
+                else:
+                    self.characters[i].speed_y += 1
+
+            self.characters[i].energy = max(self.characters[i].energy, 300)
+            self.characters[i].remaining_frame -= 1
+            if self.characters[i].remaining_frame <= 0:
+                match self.characters[i].action:
+                    case Action.CHANGE_DOWN:
+                        break
+                    case Action.DOWN:
+                        break
+                    case Action.AIR:
+                        break
+                    case Action.CROUCH:
+                        break
+                    case _:
+                        break
+
+
 
 
 
