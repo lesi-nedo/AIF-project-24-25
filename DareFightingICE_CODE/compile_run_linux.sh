96f51e86ed9b0d1aefba55b0d9245252926852c3
Oleksiy Nedobiychuk <oleksiy12345@live.it>
2025-01-13 19:21:39 +0100

    feat(headless)

--- DareFightingICE_CODE/compile_run_linux.sh
+++ DareFightingICE_CODE/compile_run_linux.sh
@@ -4,4 +4,5 @@
 javac -d bin -cp "lib/*:lib/lwjgl/*:lib/lwjgl/natives/linux/amd64/*" @sources.txt
 
 # Run the program with the LWJGL libraries in the classpath
-java -cp "bin:lib/*:lib/lwjgl/*:lib/lwjgl/natives/linux/amd64/*" Main --limithp 400 400 --grey-bg --pyftg-mode #--headless-mode
+java -cp "bin:lib/*:lib/lwjgl/*:lib/lwjgl/natives/linux/amd64/*" Main --limithp 400 400 --grey-bg --pyftg-mode --headless-mode
+--headless-mode
